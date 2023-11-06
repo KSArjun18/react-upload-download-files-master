556f435a24e8471cdba126a386759d5206a4df8a
KSArjun18 <arjunkishore18@gmail.com>
2023-11-06 12:03:03 +0530

    final

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(express.static(path.join(__dirname, '..', 'build')));
 app.use(fileRoute);
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));npm run 
+  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'))
 });
 
 app.listen(3030, () => {
